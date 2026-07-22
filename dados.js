168c79dd0462a5a8435f2d2ab12795a0d75579a1
Abdalla A. <91617960+Abdallah0101@users.noreply.github.com>
2026-07-21 21:19:39 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -26,7 +26,7 @@ const CONFIG = {
       idade: 10,
       emoji: "🦋",
       cor: "#E85D9E",          // rosa
-      pontos: 2,
+      pontos: 3,
       meta: 30,
       premio: "Chocolate grande 🍫",
       acoes: [
