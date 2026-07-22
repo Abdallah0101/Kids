be84668b1776a99ad78ab5b255088e8dbbc7afb4
Abdalla A. <91617960+Abdallah0101@users.noreply.github.com>
2026-07-22 20:22:42 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-/* ============================================================
+a/* ============================================================
    👨‍👩‍👧‍👦 PAINEL DO PAPAI — SÓ EDITE ESTE ARQUIVO!
    ------------------------------------------------------------
    Como avaliar alguém:
@@ -26,7 +26,7 @@ const CONFIG = {
       idade: 10,
       emoji: "🦋",
       cor: "#E85D9E",          // rosa
-      pontos: 21,
+      pontos: 22,
       meta: 30,
       premio: "Chocolate grande 🍫",
       acoes: [
@@ -42,7 +42,7 @@ const CONFIG = {
       idade: 6,
       emoji: "🦁",
       cor: "#F5921B",          // laranja
-      pontos: 21,
+      pontos: 22,
       meta: 30,
       premio: "Brinquedo surpresa 🎁",
       acoes: [
