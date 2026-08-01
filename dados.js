0aa6c07450259aa4620d45759ebf33c473f9ba11
Kimi Claw <kimi@claw.ai>
2026-08-01 20:48:36 +0800

    estrelas: Maryam 20 pontos

--- dados.js
+++ dados.js
@@ -26,7 +26,7 @@ const CONFIG = {
       idade: 10,
       emoji: "🦋",
       cor: "#E85D9E",          // rosa
-      pontos: 15,
+      pontos: 20,
       meta: 30,
       premio: "Chocolate grande 🍫",
       acoes: [
